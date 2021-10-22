5b34d3afabdaea406049ee0beeae899e228ca77c
princeraj863 <princeraj863@gmail.com>
2021-10-23 00:56:47 +0530

    stored user data on online database using express and mongoose using promise

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,14 +1,37 @@
 const express = require("express");
+const User = require("../model/userSchema");
 const router = express.Router();
+
+require("../db/conn");
 
 router.get("/", (req, res) => {
   res.send("hello");
 });
 
 router.post("/register", (req, res) => {
-  console.log("req.body==", req.body);
-  res.json({ message: req.body });
-  //res.send("regsiter page");
+  const { name, email, phone, password, cpassword } = req.body;
+
+  if (!name || !email || !phone || !password || !cpassword) {
+    return res.status(422).json({ error: "plz fill the feild " });
+  }
+
+  User.findOne({ email: email })
+    .then((userExist) => {
+      if (userExist) {
+        return res.status(422).json({ error: "User already exsit " });
+      }
+      const user = new User({ name, email, phone, password, cpassword }); // if key and value are same we can write anyone of them only
+
+      user
+        .save()
+        .then(() => {
+          res.status(201).json({ message: "user registered successfully" });
+        })
+        .catch((err) => res.status(500).json({ error: "failed to register" }));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
