daae124dfc229f91e35923626d543f39626651db
princeraj863 <princeraj863@gmail.com>
2021-10-26 13:10:53 +0530

    added login system with jwt and stored cookie

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,44 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 
 const Login = () => {
+  const history = useHistory();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginUser = async (e) => {
+    e.preventDefault();
+    const res = await fetch("http://localhost:5000/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    const data = res.json();
+
+    if (res.status === 400 || !data) {
+      window.alert("Invalid credentials");
+    } else {
+      window.alert("login successful");
+      history.push("/");
+    }
+  };
   return (
     <div className="form">
-      <div>
-        <label htmlFor="email">Email:</label>
-        <br />
-        <input
-          className="input"
-          type="email"
-          name="email"
-          id="email"
-          autoComplete="off"
-          placeholder="Your email"
-        />
-      </div>
+      <form>
+        <div method="POST">
+          <label htmlFor="email">Email:</label>
+          <br />
+          <input
+            className="input"
+            type="email"
+            name="email"
+            id="email"
+            autoComplete="off"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Your email"
+          />
+        </div>
 
-      <div>
-        <label htmlFor="password">password:</label>
-        <br />
-        <input
-          className="input"
-          type="password"
-          name="password"
-          id="password"
-          autoComplete="off"
-          placeholder="Your password"
-        />
-      </div>
+        <div>
+          <label htmlFor="password">password:</label>
+          <br />
+          <input
+            className="input"
+            type="password"
+            name="password"
+            id="password"
+            autoComplete="off"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Your password"
+          />
+        </div>
 
-      <div>
-        <input
-          className="input"
-          type="submit"
-          name="signin"
-          id="signin"
-          value="login"
-          autoComplete="off"
-        />
-      </div>
+        <div>
+          <input
+            className="input"
+            type="submit"
+            name="signin"
+            id="signin"
+            value="login"
+            onClick={loginUser}
+            autoComplete="off"
+          />
+        </div>
+      </form>
     </div>
   );
 };
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 const Signup = () => {
+  const history = useHistory();
   const [user, setUser] = useState({
     name: "",
     email: "",
@@ -18,84 +19,131 @@ const Signup = () => {
     setUser({ ...user, [name]: value }); //here [name] means name value i.e email, phone
   };
 
+  const PostData = async (e) => {
+    e.preventDefault();
+    const { name, email, phone, password, cpassword } = user;
+    console.log("user===", user);
+
+    const res = await fetch("http://localhost:5000/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        // as server doesn't understand json only so stringify used
+        name,
+        email,
+        phone,
+        password,
+        cpassword,
+      }),
+    });
+    console.log("res===", res.status);
+    const data = await res.json();
+    console.log("data1===", data.status);
+    if (res.status === 422 || !data) {
+      // as we are retuning 422 status when registration is not successful
+      window.alert("Invalid Registration");
+      console.log("invalid registration");
+    } else {
+      window.alert("Registration successful");
+      console.log(" registration successful");
+
+      history.push("/login"); //after registration user will go directly to login page
+    }
+  };
+
   return (
     <div className="form">
-      <div>
-        <label htmlFor="name">Name:</label>
-        <br />
-        <input
-          className="input"
-          type="text"
-          name="name"
-          id="name"
-          autoComplete="off"
-          value={user.name}
-          onChange={handleInputs}
-          placeholder="Your name"
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <br />
-        <input
-          className="input"
-          type="email"
-          name="email"
-          id="email"
-          autoComplete="off"
-          value={user.email}
-          onChange={handleInputs}
-          placeholder="Your email"
-        />
-      </div>
-      <div>
-        <label htmlFor="phone">Phone:</label>
-        <br />
-        <input
-          className="input"
-          type="number"
-          name="phone"
-          id="phone"
-          autoComplete="off"
-          value={user.phone}
-          onChange={handleInputs}
-          placeholder="Your phone"
-        />
-      </div>
-      <div>
-        <label htmlFor="password">password:</label>
-        <br />
-        <input
-          className="input"
-          type="password"
-          name="password"
-          id="password"
-          autoComplete="off"
-          value={user.password}
-          onChange={handleInputs}
-          placeholder="Your password"
-        />
-      </div>
-      <div>
-        <label htmlFor="cpassword">Confirm password:</label>
-        <br />
-        <input
-          className="input"
-          type="password"
-          name="cpassword"
-          id="cpassword"
-          autoComplete="off"
-          value={user.cpassword}
-          onChange={handleInputs}
-          placeholder="Your confirm password"
-        />
-      </div>
+      <form method="POST">
+        <div>
+          <label htmlFor="name">Name:</label>
+          <br />
+          <input
+            className="input"
+            type="text"
+            name="name"
+            id="name"
+            autoComplete="off"
+            value={user.name}
+            onChange={handleInputs}
+            placeholder="Your name"
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <br />
+          <input
+            className="input"
+            type="email"
+            name="email"
+            id="email"
+            autoComplete="off"
+            value={user.email}
+            onChange={handleInputs}
+            placeholder="Your email"
+          />
+        </div>
+        <div>
+          <label htmlFor="phone">Phone:</label>
+          <br />
+          <input
+            className="input"
+            type="number"
+            name="phone"
+            id="phone"
+            autoComplete="off"
+            value={user.phone}
+            onChange={handleInputs}
+            placeholder="Your phone"
+          />
+        </div>
+        <div>
+          <label htmlFor="password">password:</label>
+          <br />
+          <input
+            className="input"
+            type="password"
+            name="password"
+            id="password"
+            autoComplete="off"
+            value={user.password}
+            onChange={handleInputs}
+            placeholder="Your password"
+          />
+        </div>
+        <div>
+          <label htmlFor="cpassword">Confirm password:</label>
+          <br />
+          <input
+            className="input"
+            type="password"
+            name="cpassword"
+            id="cpassword"
+            autoComplete="off"
+            value={user.cpassword}
+            onChange={handleInputs}
+            placeholder="Your confirm password"
+          />
+        </div>
 
-      <div>
-        <Link className="submit" to="/login">
-          Login
-        </Link>
-      </div>
+        <div>
+          <input
+            className="input"
+            type="submit"
+            name="signup"
+            id="signup"
+            value="regsiter"
+            onClick={PostData}
+          />
+        </div>
+
+        <div>
+          <Link className="submit" to="/login">
+            Already registered
+          </Link>
+        </div>
+      </form>
     </div>
   );
 };
